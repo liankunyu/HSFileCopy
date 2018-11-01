4fbce062767ffec58be36b35c2a5bb0022659c3a
liankunyu <liankunyu@outlook.com>
2018-11-01 11:11:20 +0800

    添加项目文件。

---
+++ HSFileCopy/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace HSFileCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtObjectFile = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.txtOutMsg = new System.Windows.Forms.TextBox();
+            this.txtSourceFile = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(283, 473);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(200, 60);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "生成";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 107);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "目标文件名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(448, 61);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "起始：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(444, 107);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "结束：";
+            // 
+            // txtObjectFile
+            // 
+            this.txtObjectFile.Location = new System.Drawing.Point(226, 104);
+            this.txtObjectFile.Name = "txtObjectFile";
+            this.txtObjectFile.Size = new System.Drawing.Size(197, 30);
+            this.txtObjectFile.TabIndex = 4;
+            this.txtObjectFile.Text = "mode";
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(507, 58);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(80, 30);
+            this.txtStart.TabIndex = 5;
+            this.txtStart.Text = "1";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(507, 104);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(80, 30);
+            this.txtEnd.TabIndex = 6;
+            this.txtEnd.Text = "20";
+            // 
+            // txtOutMsg
+            // 
+            this.txtOutMsg.Location = new System.Drawing.Point(82, 176);
+            this.txtOutMsg.Multiline = true;
+            this.txtOutMsg.Name = "txtOutMsg";
+            this.txtOutMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutMsg.Size = new System.Drawing.Size(609, 290);
+            this.txtOutMsg.TabIndex = 7;
+            // 
+            // txtSourceFile
+            // 
+            this.txtSourceFile.Location = new System.Drawing.Point(226, 58);
+            this.txtSourceFile.Name = "txtSourceFile";
+            this.txtSourceFile.Size = new System.Drawing.Size(197, 30);
+            this.txtSourceFile.TabIndex = 9;
+            this.txtSourceFile.Text = "lasttime.xml";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(135, 61);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "源文件名：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.txtSourceFile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtOutMsg);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.txtObjectFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreate);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtObjectFile;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.TextBox txtOutMsg;
+        private System.Windows.Forms.TextBox txtSourceFile;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ HSFileCopy/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HSFileCopy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            int temp1, temp2;
+            string str, str1;
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo("Object");
+                FileSystemInfo[] fileinfo = dir.GetFileSystemInfos();  //返回目录中所有文件和子目录
+                foreach (FileSystemInfo i in fileinfo)
+                {
+                    if (i is DirectoryInfo)            //判断是否文件夹
+                    {
+                        DirectoryInfo subdir = new DirectoryInfo(i.FullName);
+                        subdir.Delete(true);          //删除子目录和文件
+                    }
+                    else
+                    {
+                        File.Delete(i.FullName);      //删除指定文件
+                    }
+                }
+                txtOutMsg .Text+= "Object文件夹已清空！\r\n";
+
+                int.TryParse(txtStart.Text.ToString().Trim(), out temp1);
+                int.TryParse(txtEnd.Text.ToString().Trim(), out temp2);
+                str = txtSourceFile.Text.ToString().Trim();
+                str1 = txtObjectFile.Text.ToString().Trim();
+                for (int i = temp1; i <= temp2; i++)
+                {
+                    str1 = str1 + i.ToString() + ".xml";
+                    str1= Path.Combine("Object", str1);
+                    //c#实现把一个文件从一个文件夹复制到另外一个文件夹并改名
+                    File.Copy(str, str1, true);//允许覆盖目的地的同名文件
+                    txtOutMsg.Text += str1 + "生成成功！\r\n";
+                    str1 = txtObjectFile.Text.ToString().Trim();
+                }
+            }
+            catch (Exception ex)
+            {
+                txtOutMsg.Text += ex.ToString() + "\r\n";
+            }
+        }
+    }
+}
